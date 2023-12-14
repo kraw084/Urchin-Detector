ba9329023af11772f66b45eafc1aa66ae6908175
kraw084 <kraw084@aucklanduni.ac.nz>
2023-12-14 16:42:38 +1300

    reorganised data file structure and renamed python scripts

---
+++ data/python/format_datasets.py
@@ -0,0 +1,85 @@
+import csv
+import ast
+
+"""Formats the SQ+ csv files"""
+
+def main(csv_file_path, source_name, formated_csv_name):
+    csv_file = open(csv_file_path, "r")
+    reader = csv.DictReader(csv_file)
+
+    #used to store the rows for the new csv
+    image_data_dict = {}
+
+    box_count = 0
+    polygon_count = 0
+
+    for i, row in enumerate(reader):
+        url = row["point.media.path_best"]
+        label = row["label.name"]
+
+        #ignore annotations that are not urchins or empty images
+        if label not in ["", "Evechinus chloroticus", "Centrostephanus rodgersii"]: continue
+
+        if url not in image_data_dict: #if this is the first time the image is encounted create a new entry
+            name = url.split("/")[-1]
+            campaign_name = row["point.media.deployment.campaign.name"]
+            deployment_name = row["point.media.deployment.name"]
+            depth = row["point.pose.dep"]
+            lat = row["point.pose.lat"]
+            lon = row["point.pose.lon"]
+            timestamp = row["point.pose.timestamp"]
+ 
+            image_data = {"id":i, "url": url, "name":name, "source":source_name, "deployment": deployment_name, 
+                        "campaign": campaign_name, "latitude": lat, "longitude": lon, 
+                        "depth": depth, "time": timestamp, "boxes":[]}
+            
+            image_data_dict[url] = image_data
+        
+        #if the label is an urchin and the point has a bounding polygon
+        if label and row["point.data.polygon"]:
+            confidence = float(row["likelihood"])
+            x = float(row["point.x"])
+            y = float(row["point.y"])
+
+            #squidle stores polygon points relative to the center point and the image dimensions e.g. actual point is (x, y) + (w * polygonPointX, h * polygonPointY)
+            points = ast.literal_eval(row["point.data.polygon"])
+
+            if len(points) == 4: #polygon is box
+                #squidle stores bounding boxes as point offsets, relative to the center point
+                #point order is BL, TL, TR, BR
+                box_width = points[3][0] * 2
+                box_height = points[0][1] * 2
+                box = (label, confidence, x, y, box_width, box_height)
+                box_count += 1
+            else: #polygon is not a box
+                xValues = [p[0] for p in points]
+                yValues = [p[1] for p in points]
+                box_width = max(xValues) + abs(min(xValues))
+                box_height = max(yValues) + abs(min(yValues))
+                box = (label, confidence, x, y, box_width, box_height)
+                polygon_count += 1
+
+            (image_data_dict[url])["boxes"].append(box)
+
+    newRows = []
+    for url in image_data_dict:
+        newRows.append(image_data_dict[url])
+
+    csv_file.close()
+
+    #saving the new dataset
+    formated_csv_file = open(formated_csv_name, "w", newline="")
+    writer = csv.DictWriter(formated_csv_file, newRows[0].keys())
+    writer.writeheader()
+    writer.writerows(newRows)
+    formated_csv_file.close()
+
+    print("-------- FINISHED --------")
+    print(f"Entries created: {len(image_data_dict)}")
+    print(f"Boxes found: {box_count}")
+    print(f"Polygons found: {polygon_count}")
+    print(f"Boxes is formatted dataset: {box_count + polygon_count}")
+    print(f"Images with no urchins: {sum([1 for img in newRows if not img['boxes']])}")
+
+if __name__ == "__main__":
+    main()

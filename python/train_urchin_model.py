2ba4270c36adca41f948b2e8062c4100bb047898
kraw084 <110000147+kraw084@users.noreply.github.com>
2023-12-18 16:31:17 +1300

    model train on new dataset

--- python/train_urchin_model.py
+++ python/train_urchin_model.py
@@ -1,11 +1,17 @@
-import sys
-import os
 from urchin_utils import project_sys_path, DATASET_YAML_PATH
 
 project_sys_path()
-import yolov5
-
+import yolov5.train
+import yolov5.val
 
 if __name__ == "__main__":
-    yolov5.train.run(img = 640, epochs = 150, data = DATASET_YAML_PATH, weights = "yolov5s.pt", save_period = 40)
-    #yolov5.val.run(data = dataPath, weights = "yolov5/runs/train/exp4/weights/best.pt", task = "val")
+    if False:
+        yolov5.train.run(img = 640, 
+                        epochs = 150, 
+                        data = DATASET_YAML_PATH, 
+                        weights = "yolov5s.pt", 
+                        save_period = 40,
+                        cache = "ram",
+                        batch_size = 32)
+    
+    yolov5.val.run(data = DATASET_YAML_PATH, weights = "yolov5/runs/train/exp2/weights/best.pt", task = "val")

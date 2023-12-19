4fb8eaa79d04b1a519be98b3b4f460761ad83322
kraw084 <kraw084@aucklanduni.ac.nz>
2023-12-19 16:39:01 +1300

    get metrics by metadata var or func

--- python/error_analysis.py
+++ python/error_analysis.py
@@ -10,8 +10,16 @@ from yolov5.utils.metrics import ap_per_class
 
 def get_metrics(model, image_set):
     """Computes metrics of provided image set
-        model: model to get predictions from
-        image_set: list of image paths"""
+       Arguments:
+                model: model to get predictions from
+                image_set: list of image paths
+       Returns: 
+                precision, mean precision, recall, mean recall, f1 score, ap50, 
+                map50, ap, and map of the provided images and ap_classes, a list
+                with class indices that occurs in the given image set e.g. [], [0], [1], [0, 1]
+        """
+
+    print(f"Calculating metrics on {len(image_set)} images")
 
     gt_boxes = [ast.literal_eval(row["boxes"]) for row in urchin_utils.get_dataset_rows()]
     pred_boxes = model(image_set).tolist()
@@ -62,7 +70,7 @@ def get_metrics(model, image_set):
     ap50, ap = ap_across_iou_th[:, 0], ap_across_iou_th.mean(1)
     mean_precision, mean_recall, map50, map = precision.mean(), recall.mean(), ap50.mean(), ap.mean()
 
-    return precision, mean_precision, recall, mean_recall, f1, ap50, map50, ap, map
+    return precision, mean_precision, recall, mean_recall, f1, ap50, map50, ap, map, ap_class
 
 def print_metrics(precision, mean_precision, recall, mean_recall, f1, ap50, map50, ap, map):
     cols = {"P": [precision[0], precision[1], mean_precision],
@@ -80,13 +88,39 @@ def print_metrics(precision, mean_precision, recall, mean_recall, f1, ap50, map5
 
     print(df.to_string(index=True, index_names=False))
 
+def metrics_by_var(model, images_txt, var_name, var_func = None):
+    f = open(images_txt, "r")
+    image_paths = [line.strip("\n") for line in f.readlines()]
+    f.close()
+
+    dataset_rows = urchin_utils.get_dataset_rows()
+    splits = {}
+
+    for image_path in image_paths:
+        id = urchin_utils.id_from_im_name(image_path)
+        value = dataset_rows[id][var_name]
+
+        if var_func: value = var_func(value)
+
+        if value in splits:
+            splits[value].append(image_path)
+        else:
+            splits[value] = [image_path]
+
+    
+    print(f"Getting metrics by {var_name}{' and ' + var_func.__name__ if var_func else ''}")
+    print(f"Values: {', '.join(splits.keys())}")
+    print("----------------------------------------------------")
+    for value in splits:
+        print(f"Metrics for {value} ({len(splits[value])} images):")
+        metrics = get_metrics(model, splits[value])
+        print_metrics(*metrics)
+        print("----------------------------------------------------")
+    print("FINISHED")
+
+
 if __name__ == "__main__":
     model = urchin_utils.load_model(urchin_utils.WEIGHTS_PATH, False)
-    metrics = get_metrics(model, 
-                ["C:\\Users\\kelha\\Documents\\Uni\\Summer Research\\Urchin-Detector\\data\\images\\im81.JPG",
-                 "C:\\Users\\kelha\\Documents\\Uni\\Summer Research\\Urchin-Detector\\data\\images\\im128.JPG",
-                 "C:\\Users\\kelha\\Documents\\Uni\\Summer Research\\Urchin-Detector\\data\\images\\im2436.JPG",
-                 "C:\\Users\\kelha\\Documents\\Uni\\Summer Research\\Urchin-Detector\\data\\images\\im3796.JPG"])
 
-    print_metrics(*metrics)
+    metrics_by_var(model, "data/datasets/full_dataset_v2/val.txt", "campaign")
 

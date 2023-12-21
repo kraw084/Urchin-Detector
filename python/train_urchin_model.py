e4830bc7292b531973aff9fb1a2ce92a7fc44a2b
kraw084 <110000147+kraw084@users.noreply.github.com>
2023-12-21 13:10:20 +1300

    Model train on higher res images

--- python/train_urchin_model.py
+++ python/train_urchin_model.py
@@ -5,13 +5,12 @@ import yolov5.train
 import yolov5.val
 
 if __name__ == "__main__":
-    if False:
-        yolov5.train.run(img = 640, 
-                        epochs = 150, 
-                        data = DATASET_YAML_PATH, 
-                        weights = "yolov5s.pt", 
-                        save_period = 40,
-                        cache = "ram",
-                        batch_size = 32)
+    yolov5.train.run(img = 1280, 
+                    epochs = 150, 
+                    data = DATASET_YAML_PATH, 
+                    weights = "yolov5m.pt", 
+                    save_period = 40,
+                    cache = "ram",
+                    batch_size = 32)
     
-    yolov5.val.run(data = DATASET_YAML_PATH, weights = WEIGHTS_PATH, task = "val")
+    #yolov5.val.run(data = DATASET_YAML_PATH, weights = WEIGHTS_PATH, task = "val")

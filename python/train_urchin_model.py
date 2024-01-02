7c176f643083c614252d091bca23c4a21e81b978
kraw084 <110000147+kraw084@users.noreply.github.com>
2024-01-03 10:52:10 +1300

    Yolov5m model

--- python/train_urchin_model.py
+++ python/train_urchin_model.py
@@ -5,12 +5,14 @@ import yolov5.train
 import yolov5.val
 
 if __name__ == "__main__":
-    yolov5.train.run(img = 1280, 
-                    epochs = 150, 
-                    data = DATASET_YAML_PATH, 
-                    weights = "yolov5m.pt", 
-                    save_period = 40,
-                    cache = "ram",
-                    batch_size = 32)
+    #yolov5.train.run(img = 1280, 
+    #                epochs = 150, 
+    #                data = DATASET_YAML_PATH, 
+    #                weights = "yolov5m.pt", 
+    #                save_period = 40,
+    #                cache = "ram",
+    #                batch_size = 32)
     
-    #yolov5.val.run(data = DATASET_YAML_PATH, weights = WEIGHTS_PATH, task = "val")
+    yolov5.val.run(data = DATASET_YAML_PATH, weights = "models/yolov5m/weights/best.pt", task = "train")
+    yolov5.val.run(data = DATASET_YAML_PATH, weights = "models/yolov5m/weights/best.pt", task = "val")
+    yolov5.val.run(data = DATASET_YAML_PATH, weights = "models/yolov5m/weights/last.pt", task = "val")

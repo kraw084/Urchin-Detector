b061d73338707eafb63a2a346ce8c71c4ae83b3c
kraw084 <kraw084@aucklanduni.ac.nz>
2024-01-08 12:17:55 +1300

    Moved functions from detect.py to error_analysis.py

--- python/error_analysis.py
+++ python/error_analysis.py
@@ -209,15 +209,63 @@ def train_val_metrics(model, dataset_path):
         print_metrics(*metrics)
 
 
-if __name__ == "__main__":
-    model = urchin_utils.load_model(urchin_utils.WEIGHTS_PATH, True)
-    f = open("data/datasets/full_dataset_v2/val.txt", "r")
-    image_paths = [line.strip("\n") for line in f.readlines()]
-    f.close()
+def compare_to_gt(model, txt_of_im_paths, label = "urchin", save_path = False, limit = None):
+    """Creates figures to compare model predictions to the actual labels
+        model: yolo model to run
+        txt_of_im_paths: path of a txt file containing image paths
+        label: "all", "empty", "urchin", "kina", "centro", used to filter what kind of images are compared
+        save_path: if this is a file path, figures will be saved instead of shown
+        limit: number of figures to show/save, leave as none for no limit
+    """
+    if label not in ("all", "empty", "urchin", "kina", "centro"):
+        raise ValueError(f'label must be in {("all", "empty", "urchin", "kina", "centro")}')
 
-    metrics = get_metrics(model, image_paths)
-    print_metrics(*metrics)
-    print("-------------------------------------------")
-    metrics = get_metrics(model, image_paths, tta=True)
-    print_metrics(*metrics)
+    rows = get_dataset_rows()
+
+    txt_file = open(txt_of_im_paths, "r")
+    im_paths = txt_file.readlines()
+
+    for im_path in im_paths:
+        id = id_from_im_name(im_path)
+        boxes = ast.literal_eval(rows[id]["boxes"])
+
+        if label == "empty":
+            if boxes: continue
+        elif label == "urchin":
+            if not boxes: continue
+        elif label == "kina":
+            if not boxes or boxes[0][0] == "Centrostephanus rodgersii": continue
+        elif label == "centro":
+            if not boxes or boxes[0][0] == "Evechinus chloroticus": continue
+
+        matplotlib.use('TkAgg')
+        fig = plt.figure(figsize=(14, 8))
+        im = img.imread(im_path.strip("\n"))
+
+        #plot ground truth boxes
+        ax = fig.add_subplot(1, 2, 1)
+        plt.title("Ground truth")
+        plt.imshow(im)
+        draw_bboxes(ax, boxes, im)
+            
+        #plot predicted boxes
+        ax = fig.add_subplot(1, 2, 2)
+        plt.title("Prediction")
+        plt.imshow(im)
+        prediction = batch_inference(model, [im_path.strip("\n")])[0].pandas().xywh[0]
+        draw_bboxes(ax, prediction, im)
+
+        if not save_path:
+            plt.show()
+        else:
+            fig.savefig(f"{save_path}/fig-{id}.png", format="png", bbox_inches='tight')
+
+        if limit:
+            limit -= 1
+            if limit <= 0: break
+
+
+
+if __name__ == "__main__":
+    pass
 

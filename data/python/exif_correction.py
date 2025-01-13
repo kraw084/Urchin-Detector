faf41b26e5b1bd6f9f36a54ac15e7670caa576d4
kraw084 <kraw084@aucklanduni.ac.nz>
2025-01-14 10:19:21 +1300

    updated exif_correction.py

--- data/python/exif_correction.py
+++ data/python/exif_correction.py
@@ -2,32 +2,28 @@ from PIL import Image
 from PIL import ImageOps
 import os
 
-dir_path = "data/images_helio"
+def correct_exif(image_dir):
+    """Rotates images with exif data and removes it from the file
+    Args:
+        image_dir: path to the directory where the images are stored
+    """
+    for im_path in os.listdir(image_dir):
+        #open image and read exif data
+        im = Image.open(f"{image_dir}/{im_path}", formats=["JPEG"])
+        exif_data = im.getexif()
+        
+        #check if image has exif rotation data
+        if exif_data and exif_data[274] != 1:
+            #rotate image acording to exif data and then strip it
+            print(f"Removing EXIF data ({exif_data[274]}) from {im_path}")
+            im = ImageOps.exif_transpose(im)
+            im_data = list(im.getdata())
+            new_im = Image.new(im.mode, im.size)
+            new_im.putdata(im_data)
 
-count = 0
-for im_path in os.listdir(dir_path):
-    im = Image.open(f"{dir_path}/{im_path}", formats=["JPEG"])
-    exif_data = im.getexif()
-    if exif_data:
-        orientation = exif_data[274]
-        if orientation != 1:
-            count += 1
-    im.close()
-print(count)
-
-for im_path in os.listdir(dir_path):
-    im = Image.open(f"{dir_path}/{im_path}", formats=["JPEG"])
-    exif_data = im.getexif()
-    if exif_data and exif_data[274] != 1:
-        im = ImageOps.exif_transpose(im)
-
-        print(f"Removing EXIF data ({exif_data[274]}) for {im_path}")
-        im_data = list(im.getdata())
-        new_im = Image.new(im.mode, im.size)
-        new_im.putdata(im_data)
-
-        new_im.save(f"{dir_path}/{im_path}")
+            new_im.save(f"{image_dir}/{im_path}")
         
     im.close()
 
-count = 0
+if __name__ == "__main__":
+    correct_exif("data/images")
